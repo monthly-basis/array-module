b31a6de16e21b77e810e701a3b37fc464b179576
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-02-23 03:49:52 +0000

    Write and test array path exists service

--- src/Service/PathExists.php
+++ src/Service/PathExists.php
@@ -3,8 +3,27 @@ namespace LeoGalleguillos\ArrayModule\Service;
 
 class PathExists
 {
-    public function doesPathExist(): bool
-    {
+    public function doesPathExist(
+        array $path,
+        array $array
+    ): bool {
+        if (count($path) == 1) {
+            return array_key_exists($path[0], $array);
+        }
+
+        if (array_key_exists($path[0], $array)) {
+            $key = array_shift($path);
+
+            if (!is_array($array[$key])) {
+                return false;
+            }
+
+            return $this->doesPathExist(
+                $path,
+                $array[$key]
+            );
+        }
+
         return false;
     }
 }
--- test/Service/PathExistsTest.php
+++ test/Service/PathExistsTest.php
@@ -13,9 +13,73 @@ class PathExistsTest extends TestCase
 
     public function testInitialize()
     {
-        $this->assertInstanceOf(
-            ArrayModuleService\PathExists::class,
-            $this->pathExistsService
+        $array = [
+            'a' => 1,
+            'c' => [
+                'd' => null
+            ],
+            'e' => 12345,
+            'f' => [
+                'g' => null,
+                2 => [
+                    'i' => new \stdClass(),
+                ],
+            ],
+        ];
+
+        $this->assertTrue(
+            $this->pathExistsService->doesPathExist(
+                ['a'],
+                $array
+            )
+        );
+        $this->assertFalse(
+            $this->pathExistsService->doesPathExist(
+                [0],
+                $array
+            )
+        );
+        $this->assertFalse(
+            $this->pathExistsService->doesPathExist(
+                ['b'],
+                $array
+            )
+        );
+        $this->assertTrue(
+            $this->pathExistsService->doesPathExist(
+                ['c', 'd'],
+                $array
+            )
+        );
+        $this->assertFalse(
+            $this->pathExistsService->doesPathExist(
+                ['c', 0],
+                $array
+            )
+        );
+        $this->assertTrue(
+            $this->pathExistsService->doesPathExist(
+                ['f', 2, 'i'],
+                $array
+            )
+        );
+        $this->assertTrue(
+            $this->pathExistsService->doesPathExist(
+                ['f', '2', 'i'],
+                $array
+            )
+        );
+        $this->assertFalse(
+            $this->pathExistsService->doesPathExist(
+                ['f', '2', 'j'],
+                $array
+            )
+        );
+        $this->assertFalse(
+            $this->pathExistsService->doesPathExist(
+                ['f', '2', 'i', 'j'],
+                $array
+            )
         );
     }
 }
